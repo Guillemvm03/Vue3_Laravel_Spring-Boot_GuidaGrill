6f3ffdf0bf97c3346d1e031fd829883887ef150d
Davidmpenades <davidmpenades@gmail.com>
2023-11-20 13:26:08 +0100

    Frontend detailsTable

--- client/src/services/client/TablesService.js
+++ client/src/services/client/TablesService.js
@@ -6,5 +6,9 @@ const URL_SPRING = 'http://localhost:8080/api/'
 export default {
     getTables(){
         return Api(URL_SPRING + 'tables').get()
+    },
+    GetTableById(id) {
+        console.log(id);
+        return Api(URL_SPRING + 'tables/' + id).get();
     }
 }
